27ad5dbe3114a4b0d9ce72f1930fabe7b9f347ef
Omri Habot <oh2263@columbia.edu>
2024-11-04 17:23:40 -0500

    changed theme and changed dropdown options

--- Shiny_Vis.R
+++ Shiny_Vis.R
@@ -20,12 +20,23 @@ thematic_shiny(font = "auto")
 
 
 #airquality = data("airquality")
-airquality$Month <- as.factor(airquality$Month)
+airquality$Date = as.Date(sprintf("1973-%02d-%02d", airquality$Month, airquality$Day), format="%Y-%m-%d")
+airquality$Month <- fct_recode(as.factor(airquality$Month),
+                               "May" = "5",
+                               "June" = "6",
+                               "July" = "7",
+                               "August" = "8",
+                               "September" = "9")
 
+selection_x_data <- airquality |>
+  select(-Month, -Day)
+
+selection_y_data <- airquality |>
+  select(-Month, -Day, -Date)
 
 ui <- fluidPage(
   
-  theme = bslib::bs_theme(version = 5, bootswatch = "quartz"),
+  theme = bs_theme(bootswatch = "darkly"),
   
   titlePanel("Analyzing Airquality on Roosevelt Island 1973"),
   
@@ -36,8 +47,8 @@ ui <- fluidPage(
       "Univariate", 
       sidebarLayout(
         sidebarPanel(
-          varSelectInput("var", "Variable?", data = airquality, selected = "Wind"),
-          sliderInput("bins", "Number of Bins?", value = 40, min = 0, max =
+          varSelectInput("var", "Variable?", data = selection_y_data, selected = "Wind"),
+          sliderInput("bins", "Number of Bins?", value = 40, min = 1, max =
                         100),
         ),
         mainPanel(
@@ -51,11 +62,12 @@ ui <- fluidPage(
       "Bivariate", 
       sidebarLayout(
         sidebarPanel(
-          varSelectInput("xvar", "X Variable?", data = airquality, selected = "Wind"),
-          varSelectInput("yvar", "Y Variable?", data = airquality, selected = "Temp"),
-          checkboxGroupInput("month", "Filter by Month?",
-                             choices = levels(airquality$Month), selected =
-                               5)
+          varSelectInput("xvar", "X Variable?", data = selection_x_data, selected = "Date"),
+          varSelectInput("yvar", "Y Variable?", data = selection_y_data, selected = "Temp"),
+          checkboxGroupInput("month", "Filter by Month"
+                             , choices = levels(airquality$Month)
+                             , selected = levels(airquality$Month)
+                             )
         ), 
         mainPanel(
           plotOutput("scatterplot")
@@ -74,22 +86,25 @@ server <- function(input, output) {
   output$histogram <- renderPlot({
     
     histplot <- ggplot(data = airquality, mapping=aes(x=!!input$var)) +
-                  geom_histogram(bins=input$bins)
+                  geom_histogram(bins=input$bins) +
+                  labs(title='Histogram of Selected Weather Feature')
               
     histplot
-    
-  })
+  }, res = 100, height = 700, width = 700)
   
   output$scatterplot <- renderPlot({
-    
     airquality |>
+      drop_na() |>
       filter(Month %in% input$month) |>
       ggplot(mapping = aes(x = !!input$xvar, y = !!input$yvar)) +
-      geom_point() -> scatter
+      geom_point() + list(
+        labs(title='Scatterplot of Roosevelt Island Weather Data (1973)'),
+        if(input$xvar == "Date") geom_line()
+      ) -> scatter
     
     scatter
     
-  })
+  }, res = 100, height = 700, width = 700)
   
   
 }
