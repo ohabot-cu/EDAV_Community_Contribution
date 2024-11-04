24111320ea7c2b693cf9000a7ff6975575000538
Omri Habot <oh2263@columbia.edu>
2024-11-04 17:43:23 -0500

    added omri's code

--- Shiny_Vis.R
+++ Shiny_Vis.R
@@ -10,8 +10,6 @@ library(thematic)
 
 ### Datasets: penguins, airquality
 
-### this is a test comment
-
 
 
 # shiny app showing scatterplot on airquality dataset
@@ -34,17 +32,25 @@ selection_x_data <- airquality |>
 selection_y_data <- airquality |>
   select(-Month, -Day, -Date)
 
+penguins_csv <- "https://raw.githubusercontent.com/jcheng5/simplepenguins.R/main/penguins.csv"
+penguins <- drop_na(readr::read_csv(penguins_csv))
+# Find subset of columns that are suitable for scatter plot
+penguins_num <- penguins |> select(where(is.numeric), -Year)
+penguins_cat <- penguins |> select(where(is.character))
+
+
 ui <- fluidPage(
   
-  theme = bs_theme(bootswatch = "darkly"),
+  theme = bs_theme(bootswatch = "cerulean"),
   
-  titlePanel("Analyzing Airquality on Roosevelt Island 1973"),
+  headerPanel("Demonstration of Interactive Graphs Using Shiny"),
   
   tabsetPanel(
     type = "pills",
     
     tabPanel(
       "Univariate", 
+      titlePanel("Airquality on Roosevelt Island 1973"),
       sidebarLayout(
         sidebarPanel(
           varSelectInput("var", "Variable?", data = selection_y_data, selected = "Wind"),
@@ -60,6 +66,7 @@ ui <- fluidPage(
     
     tabPanel(
       "Bivariate", 
+      titlePanel("Airquality on Roosevelt Island 1973"),
       sidebarLayout(
         sidebarPanel(
           varSelectInput("xvar", "X Variable?", data = selection_x_data, selected = "Date"),
@@ -72,16 +79,80 @@ ui <- fluidPage(
         mainPanel(
           plotOutput("scatterplot")
         )
-
+      )
+    ),
+    
+    tabPanel("|"),
+    
+    tabPanel(
+      "Multivariate",
+      titlePanel("Penguins Data"),
+      sidebarLayout(
+        sidebarPanel(
+          checkboxGroupInput(
+            "species", "Filter by Species",
+            choices = unique(penguins$Species),
+            selected = unique(penguins$Species)
+          ),
+          checkboxGroupInput(
+            "sex", "Filter by Sex",
+            choices = unique(penguins$Sex),
+            selected = unique(penguins$Sex)
+          ),
+          checkboxGroupInput(
+            "island", "Filter by Island",
+            choices = unique(penguins$Island),
+            selected = unique(penguins$Island)
+          ),
+          varSelectInput("colorby", "Color By", penguins_cat, selected = "Species"),
+          checkboxInput("by_species", "Show colors", TRUE),
+        ),
+        mainPanel(
+          plotOutput('parcoordplot')
+        )
       )
     )
-    
   )
 )
 
 
 
 server <- function(input, output) {
+  subsetted_penguins <- reactive({
+    req(input$species)
+    req(input$island)
+    req(input$sex)
+    req(input$colorby)
+    penguins |> 
+      filter(Species %in% input$species) |> 
+      filter(Island %in% input$island) |> 
+      filter(Sex %in% input$sex) |> 
+      select(input$colorby, where(is.numeric), -Year)
+  })
+  
+  output$parcoordplot <- renderPlot({
+    data <- drop_na(subsetted_penguins())
+    p <- ggparcoord(data
+                    , scale = "std"
+                    , missing = "exclude"
+                    , alphaLines = 0.3
+                    , groupColumn = if(input$by_species) 1 else NULL
+                    , columns = 2:ncol(data)
+    ) +
+      list(
+        if (input$by_species) scale_color_brewer(palette = "Set1"),
+        xlab(''),
+        ylab('Standard Deviations'),
+        # labs(title='Parallel Coordinates Plot for Penguins Data'),
+        theme(
+          legend.position = "bottom",
+          axis.text.x = element_text(angle = 20, hjust = 1)
+        )
+      )
+    
+    p
+  }, res = 100, height = 600)
+  
   
   output$histogram <- renderPlot({
     
